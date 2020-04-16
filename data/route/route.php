47bf1142e1fba2cdb0198b62fe9e745ae447fb67
david <david@getstore.cn>
2020-04-16 10:05:03 +0800

    add missing data

---
+++ data/route/route.php
@@ -0,0 +1,83 @@
+<?php	return array (
+  'cases/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  'ketang/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=5',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  'pc/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=6',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  'about-us' => 
+  array (
+    0 => 'portal/Page/index?id=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  'cases' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  'ketang' => 
+  array (
+    0 => 'portal/List/index?id=5',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  'pc' => 
+  array (
+    0 => 'portal/List/index?id=6',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  'register$' => 'user/Register/index',
+);
